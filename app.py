4c93e390fe3623c254eb76f3c4b2bc47f7be7d40
Donn Rhys Mondano <22dmondano@gmail.com>
2023-12-01 21:48:17 -0500

    progress

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
  
 app: Flask = Flask(__name__)
  
 @app.route("/")
 def index():
     return render_template('index.html')
- 
+
+@app.route('/create-todo', methods=["GET", "POST"])
+def create_todo():
+    if request.method == "POST":
+        return render_template("success.html")
+    return render_template("create-todo.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
